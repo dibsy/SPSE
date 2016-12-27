e1858855edc7de80918c9c3a6a884ba7aaefcc3c
Dibyendu <dibyendu.coder@gmail.com>
2016-12-27 01:59:58 -0800

    Working on Project1: Twitter Bot

--- module8_further_study_and_projects/project1/twitter_bot.py
+++ module8_further_study_and_projects/project1/twitter_bot.py
@@ -1,1 +1,27 @@
+import os
 print "Twitter Bot"
+
+#tweet_raw_data = ''
+
+#tweet_command = ''
+
+#pastebin_data = ''
+
+def pastebin_activities():
+	print "PasteBin Activites"
+
+def scan_tweet():
+	global tweet_raw_data,tweet_command 
+	tweet_raw_data = '#SPSEx64 ifconfig -a'
+	tweet_command = 'uptime'
+
+def execute_command():
+	print "Executing Command:\n"
+	print tweet_command
+	pastebin_data=os.system(tweet_command)
+	print pastebin_data
+
+
+scan_tweet()
+execute_command()
+pastebin_activities()
