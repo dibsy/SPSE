3ffd55e789c2644abe63dc86a869f974b96258c4
Dibyendu <dibyendu.coder@gmail.com>
2017-01-27 11:33:19 -0800

    Adding Solutions Provided By Vivek Ramachandran

---
+++ module2_system_programming/directory_traversal_spse_sol.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# This sample code is part of the SecurityTube Python Scripting Expert 
+# course and certification Website : http://securitytube-training.com 
+# Author: Vivek Ramachandran
+import os import sys pre = '' for (path, dirs, files) in 
+os.walk(sys.argv[1]) :
+	depth_from_root = len(path.split('/'))
+	print '-'*(depth_from_root*4 +8) + ' [' + path.split('/')[-1] + 
+']'
+	for file in files :
+		print '-'*(depth_from_root*4 +12) + ' ' +file
+	#print '-'*(depth_from_root +12) + ' Files: ' + str(files)
